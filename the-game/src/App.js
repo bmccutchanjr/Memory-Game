0c12bf1f37901913c7c19dede6ccf181bb9ea526
Bill McCutchan <bmccutchanjr@gmail.com>
2018-12-15 06:00:59 -0500

    almost done

--- the-game/src/App.js
+++ the-game/src/App.js
@@ -11,36 +11,117 @@ class App extends Component
         theScore: 0
     };
 
-    // theScore = 1106;
+    theCount = 0;
+
+    randomize = () =>
+    {   // Randomly arrange the animals to make the game more than trivial
+
+        // First I need a copy of animals[] because React won't let me directly mutate properties
+        // of state
+
+        const aLen = animals.length;
+        let newArray = [];
+        for (let i=0; i<aLen; i++)
+        {   newArray[i] = animals[i]
+        }
+
+        for (let i=aLen; i>=0; i--)
+        {   // randomly select some index from the first i elements in the array
+            //
+            // i is initialized to qLen and decrements with each iteration.  A random number from 0 to 11
+            // is chosen on the first iteration, 0 to 10 on the second, and so on.
+            //
+            // Remove the element from animals[] at the randomly selected index and .push() that value to the
+            // end of the array.  The loop iterates, the range of the random number is reduced by 1 so that
+            // the next randomly selected element is one that has not been selected previously.  The values
+            // pushed to the end of the array cannot be chosen again
+
+            var randomIndex = Math.floor(Math.random() * (i));
+
+            // save the value of the element at randomIndex
+            var saveIndex = newArray[randomIndex];
+
+            // remove the randomly selected index from animals[]
+            newArray.splice(randomIndex, 1);
+
+            // and push the saved value to the end of animals[]
+            newArray.push(saveIndex);
+        }
+
+        // Now I can mutate this.state.animals[]
+        this.setState ({ animals: newArray })
+    }
 
     // incrementScore = () =>
-    // {   this.score += 1;
-    //     Title.theScore (this.score)
-    // }
-    incrementScore = () =>
-    {   
-        // alert("Score: " + this.theScore)
-        const newScore = this.state.theScore + 1;
-        this.setState({ theScore: newScore });
-        // Title.setScore (this.theScore)
+    incrementScore = (id) =>
+    {
+        // This function is used by <Card> to increment the score when a tile card has been guessed
+        // correctly.  incrementScore() is passed to <Card> in props.
+
+        // But first things first...  Reshuffling animals[] does not shuffle the <Card> components, just
+        // the data rendered in the component.  So the state property of the <Card> component no longer
+        // references the animal, but the component.  That means I can't track whether an animal has
+        // been selected in the <Card> component.  I have to do it here.
+        //
+        // <Card> now gets the id of the animal object as well as the name and image url.  When the
+        // <card> calls this function it will pass the id as a parameter so this function knows which
+        // animal was selected.
+        //
+        // Keep in mind that the data has been shuffled, and the elements are no longer in numeric order
+        // of id.  I need to find that element first...
+
+        let index = 0;
+        const aLen = this.state.animals.length;
+
+        for (let i=0; i<aLen; i++)
+        {   if (this.state.animals[i].id === id) index = i;
+        }
+// alert("id: " + id + " index: " + index);
+alert("isSelected: " + this.state.animals[index].isSelected);
+        if (this.state.animals[index].isSelected)
+        {   // This animal was selected previously...that's game over!
+            this.resetGame()
+        }
+        else
+        {
+            this.setState({ [animals[index].isSelected]: true });
+alert("s/b true: " + this.state.animals[index].isSelected);
+            // this.state.animals[index].isSelected = true;
+            this.setState({ theScore: this.state.theScore + 1 });
+
+            this.theCount += 1;
+            
+            if (this.theCount === 12)
+            {   // There are 12 cards.  The game is won if all the cards are selected.
+alert("You won!")
+                this.resetGame();
+            }
+            else
+            {   // If the game isn't over, reanrrange the cards for the next guess
+                this.randomize()
+            }
+        }
     }
 
     resetGame = () =>
-    {   this.setState({ TheScore: 0 });
-        // Title.theScore (this.score)
+    {   
+alert("resetting")
+        this.setState({ theScore: 0 });
+        this.randomize();
+        this.theCount = 0;
     }
 
     render ()
     {   return (
             <Wrapper>
-                {/* <h1 className="title">Memory Game</h1> */}
-                {/* <Title score = { 0 } /> */}
                 <Title score = { this.state.theScore } />
                 <main>
                     { this.state.animals.map(animal =>
                     (   <Card
+                            id={ animal.id }
                             name={ animal.name }
                             image={ animal.img }
+                            isSelected={ animal.isSelected }
                             incrementScore={ this.incrementScore }
                             resetGame={ this.resetGame }
                         />
--- the-game/src/components/Card/Card.js
+++ the-game/src/components/Card/Card.js
@@ -2,20 +2,35 @@ import React, { Component } from "react";
 import "./Card.css";
 
 class Card extends Component
-{   state =
-    {   isSeleted: false
-    }
+{   
+    // state =
+    // {   isSeleted: false
+    // }
     
 
     handleClick = event =>
-    {   event.preventDefault();
-        alert(`Name: ${ this.props.name }`)
+    {   // The event handler for click events on the cards.  handleClick() checks to see if the card has
+        // been clicked previously and then either resets the game (if the card has already been selected)
+        // or increments the score (if it has not)
+        event.preventDefault();
+
+        //  If the card has already been selected.  That's the end of the game...
+        // if (this.state.isSelected)
+// alert("selected: " + this.props.isSelected)
+        if (this.props.isSelected)
+            this.props.resetGame()
+        else
+        {   // If the card has not been selected, set isSecelected to true and inctrement the score
+            // this.setState({ isSelected: true })
+            // this.props.incrementScore()
+            this.props.incrementScore(this.props.id)
+        }
     }
 
     render ()
     {   return (
-            // <div className="card" onClick={ this.handleClick }>
-            <div className="card" onClick={ this.props.incrementScore }>
+            <div className="card" onClick={ this.handleClick }>
+            {/* <div className="card" onClick={ this.props.incrementScore(this.props.id) }> */}
                 <img src={ this.props.image} alt={ this.props.name}/>
                 <div className="name-div">{ this.props.name }</div>
             </div>
