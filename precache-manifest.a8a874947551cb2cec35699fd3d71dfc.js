b0ed2970840fdfe4b82b10a45f27f0b67b867ea1
Bill McCutchan <bmccutchanjr@gmail.com>
2018-12-15 23:39:51 -0500

    Updates

---
+++ precache-manifest.a8a874947551cb2cec35699fd3d71dfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1aa354a30b0dd1708cc1",
+    "url": "/Memory-Game/static/js/runtime~main.1aa354a3.js"
+  },
+  {
+    "revision": "cc4280a26589b64e289d",
+    "url": "/Memory-Game/static/js/main.cc4280a2.chunk.js"
+  },
+  {
+    "revision": "b61f9f0b70fcc34a85f9",
+    "url": "/Memory-Game/static/js/1.b61f9f0b.chunk.js"
+  },
+  {
+    "revision": "cc4280a26589b64e289d",
+    "url": "/Memory-Game/static/css/main.2d4c2be4.chunk.css"
+  },
+  {
+    "revision": "2f798beab5eb0dc9290c58f3f36a4065",
+    "url": "/Memory-Game/index.html"
+  }
+];
